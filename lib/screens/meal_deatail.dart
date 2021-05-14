cfad145de131af78795f4646152a66c65e083cd9
devesh0605 <dsaxena_be18@thapar.edu>
2021-05-14 15:40:37 +0530

    details screen completed

--- lib/screens/meal_deatail.dart
+++ lib/screens/meal_deatail.dart
@@ -1,16 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meals/models/dummy_data.dart';
 
 class MealDetail extends StatelessWidget {
   final mealId;
   MealDetail({this.mealId});
+
+  Widget headingBuilder(BuildContext context, String title) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline6,
+      ),
+    );
+  }
+
+  final ScrollController _controllerOne = ScrollController();
+  final ScrollController _controllerTwo = ScrollController();
+
   @override
   Widget build(BuildContext context) {
+    final selectedMeal =
+        DUMMY_MEALS.firstWhere((element) => element.id == mealId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('The meal id is $mealId'),
+        leading: null,
+        title: Text('${selectedMeal.title}'),
       ),
-      body: Center(
-        child: Text(mealId),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            headingBuilder(context, 'Ingredients'),
+
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.red),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              height: 200,
+              width: MediaQuery.of(context).size.width,
+              child: Scrollbar(
+                controller: _controllerOne,
+                isAlwaysShown: true,
+                showTrackOnHover: true,
+                child: ListView.builder(
+                  controller: _controllerOne,
+                  itemBuilder: (ctx, index) {
+                    return Card(
+                      color: Theme.of(context).accentColor,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 5,
+                          horizontal: 10,
+                        ),
+                        child: Text(
+                          selectedMeal.ingredients[index],
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: selectedMeal.ingredients.length,
+                ),
+              ),
+            ),
+            headingBuilder(context, 'Steps'),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.red),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              height: 200,
+              width: MediaQuery.of(context).size.width,
+              child: Scrollbar(
+                controller: _controllerTwo,
+                isAlwaysShown: true,
+                showTrackOnHover: true,
+                child: ListView.builder(
+                  controller: _controllerTwo,
+                  itemBuilder: (ctx, index) {
+                    return Column(
+                      children: [
+                        ListTile(
+                          leading: CircleAvatar(
+                            child: Text('# ${index + 1}'),
+                          ),
+                          title: Text(selectedMeal.steps[index]),
+                        ),
+                        Divider(
+                          thickness: 5,
+                        ),
+                      ],
+                    );
+                  },
+                  itemCount: selectedMeal.steps.length,
+                ),
+              ),
+            ),
+
+            //Text(selectedMeal),
+          ],
+        ),
       ),
     );
   }
